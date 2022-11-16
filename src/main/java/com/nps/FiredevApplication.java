4b6adb67c4f3b6dd3569ce2619d27dd9d83cfcb3
Abner Werley <abnerwerley77@gmail.com>
2022-11-16 18:50:41 -0300

    @abnerwerley [Initial commit]

---
+++ src/main/java/com/nps/FiredevApplication.java
@@ -0,0 +1,13 @@
+package com.nps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiredevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiredevApplication.class, args);
+	}
+
+}
