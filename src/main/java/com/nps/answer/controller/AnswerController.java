8ea4c4bcdbd6661acea072a9060254ac4d0c2966
Abner Werley <abnerwerley77@gmail.com>
2023-01-13 16:59:25 -0300

    :recycle: @abnerwerley [little adjustments so that backend can be consumed]

--- src/main/java/com/nps/answer/controller/AnswerController.java
+++ src/main/java/com/nps/answer/controller/AnswerController.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 @RestController
 @RequestMapping("/answer")
 @AllArgsConstructor
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class AnswerController {
 
     @Autowired
